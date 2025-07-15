8ef523a71cf577555f88436d7f61cf8073829813
watanabe <watanabacon0224@egmail.com>
2025-07-15 21:45:02 +0900

    PR: add memo field

--- flaskr/db.py
+++ flaskr/db.py
@@ -14,6 +14,7 @@ def create_tasks_table():
                 deadline TEXT,
                 status TEXT,
                 priority TEXT,
-                tag TEXT)
+                tag TEXT,
+                memo TEXT)
                 ''')
     con.close()
