f19f27792d39735f2e6535e5c866ad55beeac592
hanarikishi <watanabacon0224@gmail.com>
2025-07-17 21:46:16 +0900

    add : CORS

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flaskr import db, main
+from flask_cors import CORS
+
 app =Flask(__name__)
-import flaskr.main
-from flaskr import db
+CORS(app)
 db.create_tasks_table()
+
+# 今後こっちに移行するかも
+# def create_app():
+#     app = Flask(__name__)
+#     CORS(app)
+#     db.create_tasks_table()
+#     return app
