7ee5ead81451b0cc7b23db59bc3b4c00966dd5b2
hanarikishi <watanabacon0224@gmail.com>
2025-07-18 22:25:32 +0900

    vue setup

---
+++ frontend/vue-task-app/src/views/TaskForm.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h2>Task{{ $route.params.id ? '編集' : '新規作成' }}</h2>
+        <p>///form////</p>
+    </div>
+</template>
---
+++ frontend/vue-task-app/src/views/TaskList.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>タスク一覧</h2>
+    <p>ここに一覧を表示します</p>
+  </div>
+</template>
